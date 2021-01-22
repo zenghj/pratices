e4ca1fda13730228e38ac1c89276ca246b2257bf
zenghj <zenghj@outlook.com>
2021-01-22 21:36:34 +0800

    add socket.io demo

---
+++ socket.io/chat-demo/index.js
@@ -0,0 +1,31 @@
+var app = require('express')();
+var http = require('http').createServer(app);
+var io = require('socket.io')(http);
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
+app.get('/socket.io/socket.io.js', function(req, res){
+  res.sendFile(__dirname + '/node_modules/socket.io-client/dist/socket.io.js');
+});
+
+
+io.on('connection', function(socket){
+  console.log('a user connected');
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
+  });
+  socket.on('chat message', function(msg){
+    console.log('message: ' + msg);
+  });
+});
+
+setTimeout(() => {
+  io.send({
+    type: 'reload'
+  })
+}, 3000)
+
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
